554abc21bb2531048566f7c4a7084b0fbd83f048
Sadhana91296 <sadhanasingh91296@gmail.com>
2025-02-20 22:48:52 +0530

    Getting employee list data with hardcoded

--- src/main/java/com/example/SpringTest/Controller/EmployeeController.java
+++ src/main/java/com/example/SpringTest/Controller/EmployeeController.java
@@ -1,18 +1,24 @@
 package com.example.SpringTest.Controller;
 
 import com.example.SpringTest.Handler.Employee;
+import com.example.SpringTest.service.EmployeeService;
 import lombok.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 //@Controller + @ResponseBody or only @RestController
 @RestController
 public class EmployeeController {
     //@RequestMapping(value="/employees", method= RequestMethod.GET)
     //@ResponseBody
+    @Autowired
+    private EmployeeService eService;
     @GetMapping("/employees")
-    public String getEmployee()
+    public List<Employee> getEmployee()
     {
-        return "Display Employee List";
+        return eService.getEmployee();
     }
     @GetMapping("/employees/{id}")
     public String getEmployee(@PathVariable("id") Long id){
---
+++ src/main/java/com/example/SpringTest/service/EmployeeService.java
@@ -0,0 +1,9 @@
+package com.example.SpringTest.service;
+
+import com.example.SpringTest.Handler.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    List<Employee> getEmployee();
+}
